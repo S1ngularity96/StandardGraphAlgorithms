bbd540b3be14161d74fa1bc8005118e6b11bd150
Andrei Alexeev <andrei.alexeev@alumni.fh-aachen.de>
2021-04-12 23:53:23 +0200

    read graph with capacity flag

--- Application/Program.cs
+++ Application/Program.cs
@@ -10,7 +10,7 @@ namespace MA
             Graph g = new UndirectedGraph();
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            g.ReadFromFile(Config.DATA_DIR + "/Graph1.txt");
+            g.ReadFromFile(Config.DATA_DIR + "/Graph1.txt", capacity: false);
             watch.Stop();
             System.Console.WriteLine($"Sekunden vergangen: {watch.ElapsedMilliseconds / 1000}");
             System.Console.WriteLine(g);
